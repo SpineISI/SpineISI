e5007a248898d6c5956bc5f9ccb8dfd68e929486
Rocco <rocco11nicholls@gmail.com>
2016-03-24 16:43:03 -0600

    Completed the Patient Information page.

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('.dropdown-title').click(function() {
+    var list = $(this).parent().find('.dropdown-list');
+    if ($(this).hasClass('expanded')) {
+      list.show();
+      list.slideUp(300, function() {
+        list.addClass('hidden');
+      });
+      $(this).removeClass('expanded');
+    } else {
+      list.removeClass('hidden');
+      list.hide()
+      list.slideDown(300);
+      $(this).addClass('expanded');
+    }
+  })
+})
