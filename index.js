253fe04906ee56ee9a2351a99c010a4e3d3288c8
Rocco <rocco11nicholls@gmail.com>
2016-03-30 14:57:31 -0600

    Minor fixes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.dropdown-title').click(function() {
+  $('.dropdown-title').on('click tap', function() {
     var list = $(this).parent().find('.dropdown-list');
     if ($(this).hasClass('expanded')) {
       list.show();
